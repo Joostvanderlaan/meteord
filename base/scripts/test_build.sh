b97ce919b6f29f1cd0c8d22b35515446d6057987
J <joostvanderlaan@gmail.com>
2015-08-11 17:19:46 +0200

    Update test_build.sh

--- base/scripts/test_build.sh
+++ base/scripts/test_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
+bash $METEORD_DIR/lib/install_jshint_csslint.sh
 bash $METEORD_DIR/lib/install_meteor.sh
 bash $METEORD_DIR/lib/build_app.sh
-bash $METEORD_DIR/lib/run_tests.sh
